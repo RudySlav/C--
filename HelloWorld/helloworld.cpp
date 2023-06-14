5f80f9c1ff87d2293e1077406bcf6c901cefcfc7
Krzysztof Mendel <krzysztof.mendel@mycit.ie>
2023-06-14 19:51:14 +0100

    First Project, Simple Hello Print Out

---
+++ HelloWorld/helloworld.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+
+// This is a comment
+
+/*
+    This
+    is
+    a
+    multiline
+    comment
+*/
+
+    std::cout << "I like burrito!" << std::endl;
+    std::cout << "Its really good!" << '\n';
+    return 0;
+}

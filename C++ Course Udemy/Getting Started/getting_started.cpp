d6e1f3a870bb698fa22b10049066421ad9b2454e
Krzysztof Mendel <chrismendel2k@outlook.com>
2023-06-26 13:48:53 +0100

    Started on a Udemy Course for C++

---
+++ C++ Course Udemy/Getting Started/getting_started.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    
+    int favourite_number;
+    
+    std::cout << "What is your favourite number? :";
+    
+    std::cin >> favourite_number;
+
+    std::cout << "Amazing! That's my favourite number too!" << std::endl;
+
+    return 0;
+}

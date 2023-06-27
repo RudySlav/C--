c68f8ac9cc19dfbfc68fbaf1108971242ab0a55e
Krzysztof Mendel <chrismendel2k@outlook.com>
2023-06-27 14:36:28 +0100

    Learned about the structure of C++

---
+++ C++ Course Udemy/Structure C++ Program/structure.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+// This is a single line comment
+
+/*
+    This is a multiline comment
+*/
+
+    int x {3};
+
+    cout << "Hello World!";
+
+    cout << "Hello Frank!";
+
+    cout << "Sally has " << x << " dogs.";
+
+    int m {};
+    int d {};
+    int y {};
+
+    cin >> m;
+    cin >> d;
+    cin >> y;
+
+    cout << m << " " << d << " " << y;
+
+    return 0;
+}
